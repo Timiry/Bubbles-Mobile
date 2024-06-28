47e0919ecb5e51c2931fa9ff535e17d16a11c234
Timiry <irina.v.tim@gmail.com>
2024-06-28 14:31:04 +0300

    fix tooltip styles

--- src/components/Traffic/Components/TrafficItem/styles.tsx
+++ src/components/Traffic/Components/TrafficItem/styles.tsx
@@ -2,7 +2,7 @@ import { styled } from "@mui/material";
 import { LinearProgress, linearProgressClasses } from "@mui/material";
 import InfoIcon from "~/assets/icons/info.svg?react";
 import { stylesToColorSvg } from "~/theme/utils";
-import Tooltip, { tooltipClasses } from "@mui/material/Tooltip";
+import Tooltip, { tooltipClasses, TooltipProps } from "@mui/material/Tooltip";
 
 export const MuiLinearProgress = styled(LinearProgress)(({ theme }) => ({
   height: 5,
@@ -21,11 +21,18 @@ export const StyledInfoIcon = styled(InfoIcon)(({ theme }) => ({
   ...stylesToColorSvg(theme.palette.icon.secondary),
 }));
 
-export const MuiTooltip = styled(Tooltip)(({ theme }) => ({
+export const MuiTooltip = styled(({ className, ...props }: TooltipProps) => (
+  <Tooltip {...props} arrow classes={{ popper: className }} />
+))(({ theme }) => ({
   [`& .${tooltipClasses.arrow}`]: {
-    backgroundColor: theme.palette.general.information,
+    color: theme.palette.general.information,
   },
   [`& .${tooltipClasses.tooltip}`]: {
     backgroundColor: theme.palette.general.information,
+    padding: "16px",
+    font: "PF BeauSans Pro",
+    fontWeight: "bold",
+    fontStyle: "oblique",
+    borderRadius: "12px",
   },
 }));
